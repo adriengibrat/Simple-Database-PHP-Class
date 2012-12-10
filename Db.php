a33ebaf33a45d8804c24373700f6d991d7a23d13
Adrien Gibrat <adrien.gibrat@gmail.com>
2012-12-10 09:18:10 +0100

    Update Db.php

--- Db.php
+++ Db.php
@@ -1,47 +1,54 @@
 <?php
 class Db {
-	/* CONFIG */
-	protected static $config;
+	/* Configuration */
+	protected static $config = array(
+		'driver' => 'mysql',
+		'host'   => 'localhost'
+	);
 	static public function config ( $key = null, $value = null ) {
 		if ( ! isset( $key ) )
-			return self::$config;
+			return static::$config;
 		if ( isset( $value ) )
-			return self::$config[ (string) $key ] = $value;
+			return static::$config[ (string) $key ] = $value;
 		if ( is_array( $key ) )
-			return array_map( 'self::config', array_keys( (array) $key ), array_values( (array) $key ) );
-		if ( isset( self::$config[ $key ] ) )
-			return self::$config[ $key ];
+			return array_map( 'static::config', array_keys( (array) $key ), array_values( (array) $key ) );
+		if ( isset( static::$config[ $key ] ) )
+			return static::$config[ $key ];
 	}
-	/* CONSTRUCT */
+	/* Constructor */
 	protected $db;
-	private   $results;
+	protected $statement;
 	public function __construct ( $dns, $user, $pass = null ) {
 		$this->db = new pdo( $dns, $user, $pass, array(
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 		) );
 	}
-	/* STATIC */
-	private static $instance;
+	/* Static instance */
+	protected static $instance;
 	static public function instance () {
-		if ( self::$instance )
-			return self::$instance;
-		return self::$instance = new static( db::config( 'dsn' ) ?: db::config( 'dsn', db::config( 'driver' ) . ':host=' . db::config( 'host' ) . ';dbname=' . db::config( 'database' ) ), db::config( 'user' ), db::config( 'pass' ) );
+		return static::$instance ?: 
+			static::$instance = new static( 
+				static::config( 'dsn' ) ?:
+					static::config( 'dsn', static::config( 'driver' ) . ':host=' . static::config( 'host' ) . ';dbname=' . static::config( 'database' ) ), static::config( 'user' ), static::config( 'password' )
+			);
 	}
-	/* QUERY */
+	/* Query methods */
 	public function raw ( $query ) {
-		if( ! $this->results = $this->db->query( $query ) )
-			throw new Exception( 'SQL error : ' . implode( ' - ', $this->db->errorInfo() ) );
+		$this->statement = $this->db->query( $query );
 		return $this;
 	}
-	public function query ( $query, $params ) {
-		if( ! $this->results = $this->db->prepare( $query ) )
-			throw new Exception( 'SQL error : ' . implode( ' - ', $this->db->errorInfo() ) );
-		if ( ! $this->results->execute( $params ) )
-			throw new Exception( 'SQL Statement error : ' . implode( ' - ', $this->results->errorInfo() ) );
+	public function query ( $query, array $params ) {
+		$name = 'STATEMENT:' . $query;
+		if ( $config = self::config( $name ) )
+			$this->statement = $config;
+		else
+			$this->statement = self::config( $name, $this->db->prepare( $query ) );
+		$this->statement->execute( $params );
 		return $this;
 	}
+//@todo
 	public function select ( $table, $fields = '*', $where = null, $order = null, $limit = null ) {
-		$sql = 'SELECT ' . self::_fields( $fields ) . ' FROM `' . $table . '`';
+		$sql = 'SELECT ' . self::_fields( $fields ) . ' FROM ' . $this->_escape( $table );
 		if ( $where )
 			$sql .= ' WHERE ' . $where;
 		if ( $order )
@@ -50,129 +57,183 @@ class Db {
 			$sql .= ' LIMIT ' . $limit;
 		return $this->raw( $sql );
 	}
-	/* HELPERS */
+	/* Helpers methods */
 	protected function _key ( $table, $key = null ) {
 		return $key ?:
-			reset( $this->key( $table ) );
+			current( $this->key( $table ) );
 	}
-	static protected function _obj ( $class ) {
+	static protected function _fetch ( $class ) {
 		return $class ?:
-			self::config( 'obj' ) ?:
+			self::config( 'fetch' ) ?:
 				'stdClass';
 	}
-	static protected function _param ( $data, $glue = ', ' ) {
-		$keys = array_keys( (array) $data );
+	static protected function _plain ( $string ) {
+
+		return ! preg_match( '/\W/i', $string );
+	}
+	static protected function _param ( $param ) {
+		return self::_plain( $param ) ?
+			':' . $param :
+			'?';
+	}
+	static protected function _params ( $data, $operator = '=', $glue = ', ' ) {
+		$keys = is_string( $data) ? array( $data ) : array_keys( (array) $data );
 		foreach ( $keys as &$key )
-			$key = '`' . $key . '` = :' . $key
+			$key = implode( ' ', array( $this->_escape( $key ), $operator, self::_param( $key ) ) );
 		return implode( $glue, $keys );
+	}
+	static protected function _escape ( $field ) {
+		return self::_plain( $field ) ?
+			'`' . $field  . '`' :
+			$field;
 	}
 	static protected function _fields ( $fields ) {
 		if ( empty( $fields ) )
 			return '*';
 		if ( is_string( $fields ) )
 			return $fields;
-		$fields = (array) $fields; // Copy if referenced object
-		foreach ( (array) $fields as $alias => &$field )
-			$field = is_string( $alias ) ?
-				'`' . $field  . '` AS `' . $alias . '`' :
-				'`' . $field  . '`';
-		return implode( ', ', $fields );
+		$_fields = array();
+		foreach ( $fields as $alias => $field )
+			$_fields[] = self::_escape( $field ) . ( is_string( $alias ) ? ' AS `' . $alias . '`' : '' );
+		return implode( ', ', $_fields );
 	}
-	static protected function _column ( $data, $field ) {
-		$data = (array) $data; // Copy if referenced object
-		foreach ( $data as &$row )
-			if ( isset( $row->{$field} ) )
-				$row = $row->{$field};
-		return $data;
-
-
-		static $column;
-		if ( ! $column )
-			$column = function ( &$r, $i, $f ) { if ( isset( $r->{$f} ) ) $r = $r->{$f}; };
-		$data = (array) $data; // Walk on a copy
-		if ( array_walk( $data, $column, preg_replace( '/[^\w-]/', '', $field ) ) )
-			return new ArrayObject( $data );
+	static protected function _column ( array $data, $field ) {
+		$column = array();
+		foreach ( $data as $row )
+			if ( is_object( $row ) && isset( $row->{$field} ) )
+				$column[] = $row->{$field};
+			else if ( is_array( $row ) && isset( $row[ $field ] ) )
+				$column[] = $row[ $field ];
+			else 
+				$column[] = null;
+		return $column;
 	}
-	/* CRUD */
-	public function create ( $table, $data ) {
+	static protected function _index ( array $data, $field ) {
+		return array_combine(
+			self::_column( $data, $field ),
+			$data
+		);
+	}
+	static protected function _conditions ( $conditions, $glue = ' AND ' ) {
+		$sql = $params = array();
+		$i   = 0;
+		foreach ( (array) $conditions as $condition => $param ) {
+			if ( is_string( $condition ) ) {
+				for ( $keys = array(), $n = 0; $n = strpos( $condition, '?', $n ); $n ++ )
+					$condition = substr_replace( $condition, ':' . ( $keys[] = '_' . ++ $i ), $n, 1 );
+				$params += empty( $keys ) ?
+					(array) $param :
+					array_combine( $keys, (array) $param );
+			} else
+				$condition = $param;
+			$sql[]= $condition;
+		}
+		return array( implode( $glue, $sql ) => $params );
+	}
+	/* CRUD methods */
+	public function create ( $table, array $data ) {
 		$keys = array_keys( $data );
-		$sql  = 'INSERT INTO `' . $table . '` (' . implode( ', ', $keys ) . ') VALUES (:' . implode( ', :', $keys ) . ')';
+		$sql  = 'INSERT INTO ' . $this->_escape( $table ) . ' (' . implode( ', ', $keys ) . ') VALUES (:' . implode( ', :', $keys ) . ')';
 		return $this->query( $sql, $data );  
 	}
 	public function read ( $table, $id, $key = null ) {
 		$key = $this->_key( $table, $key );
-		$sql = 'SELECT * FROM `' . $table . '` WHERE `' . $key . '` = :' . $key;
+		$sql = 'SELECT * FROM ' . $this->_escape( $table ) . ' WHERE ' . self::_params( $key );
 		return $this->query( $sql, array( ':' . $key => $id ) );
 	}
-	public function update ( $table, $field, $value = null, $id = null, $key = null ) {
-		if ( is_array( $field ) || $field instanceof Traversable ) {
+	public function update ( $table, $data, $value = null, $id = null, $key = null ) {
+		if ( is_array( $data ) ) {
 			$key  = $id;
 			$id   = $value;
-			$data = $field;
 		} else
-			$data = array( $field  => $value );
+			$data = array( $data => $value );
 		$key = $this->_key( $table, $key );
 		if ( is_null( $id ) && isset( $data[ $key ] ) && ! ( $id = $data[ $key ] ) )
 			throw new Exception( 'No `' . $key . '` key value to update `' . $table . '` table, please specify a key value' );
-		$sql = 'UPDATE `' . $table . '` SET ' . self::_param( $data ) . ' WHERE `' . $key . '` = :' . $key;
+		$sql = 'UPDATE ' . $this->_escape( $table ) . ' SET ' . self::_params( $data ) . ' WHERE ' . self::_params( $key );
 		return $this->query( $sql, array_merge( $data, array( ':' . $key => $id ) ) );
 	}
 	public function delete ( $table, $id, $key = null ) {
 		$key = $this->_key( $table, $key );
-		$sql = 'DELETE FROM `' . $table . '` WHERE `' . $key . '` = :' . $key;
+		$sql = 'DELETE FROM ' . $this->_escape( $table ) . ' WHERE ' . self::_params( $key );
 		return $this->query( $sql, array( ':' . $key => $id ) );
 	}
-	/* FETCH */
+	/* Fetch methods */
 	public function fetch ( $class = null ) {
-		var_dump( $this->results );
+		if ( ! $this->statement )
+			throw new Exception( 'Can\'t fetch result if no query!' );
 		return $class === false ?
-			$this->results->fetch( PDO::FETCH_ASSOC ) :
-			$this->results->fetchObject( self::_obj( $class ) );
+			$this->statement->fetch( PDO::FETCH_ASSOC ) :
+			$this->statement->fetchObject( self::_fetch( $class ) );
 	}
 	public function all ( $class = null ) {
+		if ( ! $this->statement )
+			throw new Exception( 'Can\'t fetch results if no query!' );
 		return $class === false ?
-			$this->results->fetchAll( PDO::FETCH_ASSOC ) :
-			new ArrayObject( $this->results->fetchAll( PDO::FETCH_CLASS, self::_obj( $class ) ) );
+			$this->statement->fetchAll( PDO::FETCH_ASSOC ) :
+			$this->statement->fetchAll( PDO::FETCH_CLASS, self::_fetch( $class ) );
 	}
 	public function column ( $field, $index = null ) {
 		$data   = $this->all( false );
 		$values = self::_column( $data, $field );
-		return $index ?
-			new ArrayObject( array_combine( (array) self::_column( $data, $index ), (array) $values ) ) :
+		return is_string( $index ) ?
+			array_combine( self::_column( $data, $index ), $values ) :
 			$values;
 	}
-	/* VARIOUS */
-	public function index ( $table ) {
-		$index = 'INDEX:' . $table;
-		if ( $config = self::config( $config ) )
-			return $config;
-		if ( ! $index = $this->db->query( 'SHOW INDEX FROM `' . $table . '`', PDO::FETCH_ASSOC ) )
-			throw new Exception( 'No primary key on `' . $table . '` table, please specify a key' );
-		return self::config( $index, $index->fetchAll( false ) );
-	}
+	/* Table infos */
 	public function key ( $table ) {
-		$pk = 'PK:' . $table;
-		if ( $config = self::config( $pk ) )
+		$name = 'KEY:' . $table;
+		if ( $config = self::config( $name ) )
 			return $config;
-		if ( ! $key = $this->db->query( 'SHOW INDEX FROM `' . $table . '` WHERE `Key_name` = "PRIMARY"', PDO::FETCH_COLUMN, 4 ) )
-			throw new Exception( 'No primary key on `' . $table . '` table, please specify a key' );
-		return self::config( $pk, $key->fetchAll( false ) );
+		$sql = 'SELECT `COLUMN_NAME`
+			FROM `INFORMATION_SCHEMA`.`COLUMNS`
+			WHERE `TABLE_NAME` = ' . $this->quote( $table ) . ' AND `COLUMN_KEY` = "PRI"
+			ORDER BY `ORDINAL_POSITION` ASC';
+		$key = $this->db->query( $sql );
+		if ( ! $key->rowCount() )
+			throw new Exception( 'No primary key on `' . $table . '` table, please set a primary key' );
+		return self::config( $name, $key->fetchAll( PDO::FETCH_COLUMN, 0 ) );
 	}
-	/* @todo */
 	public function fields ( $table ) {
-
+		$name = 'FIELDS:' . $table;
+		if ( $config = self::config( $name ) )
+			return $config;
+		$sql = 'SELECT 
+			`COLUMN_NAME`                                               AS `name`, 
+			`COLUMN_DEFAULT`                                            AS `default`, 
+			NULLIF( `IS_NULLABLE`, "NO" )                               AS `null`, 
+			`DATA_TYPE`                                                 AS `type`, 
+			COALESCE( `CHARACTER_MAXIMUM_LENGTH`, `NUMERIC_PRECISION` ) AS `length`, 
+			`CHARACTER_SET_NAME`                                        AS `encoding`, 
+			`COLUMN_KEY`                                                AS `key`, 
+			`EXTRA`                                                     AS `auto`, 
+			`COLUMN_COMMENT`                                            AS `comment`
+			FROM `INFORMATION_SCHEMA`.`COLUMNS`
+			WHERE `TABLE_NAME` = ' . $this->quote( $table ) . '
+			ORDER BY `ORDINAL_POSITION` ASC';
+		$fields = $this->db->query( $sql );
+		if ( ! $fields->rowCount() )
+			throw new Exception( 'No `' . $table . '` table, please specify a valid table' );
+		return self::config( $name, self::_index( $fields->fetchAll( PDO::FETCH_CLASS ), 'name' ) );
 	}
 	public function quote ( $value ) {
-
-		return $this->db->quote( $value );
+		return is_null( $value ) ?
+			'NULL' : 
+			$this->db->quote( $value );
 	}
+	/* Statement infos */
 	public function id () {
 
 		return $this->db->lastInsertId();
 	}
-	public function error () {
-		return ! $this->results ? 
-			$this->db->errorInfo() :
-			$this->results->errorInfo() ?: null;
+	public function count () {
+		return $this->statement ? 
+			$this->statement->rowCount() :
+			null;
+	}
+	public function sql () {
+		return $this->statement ? 
+			$this->statement->queryString :
+			null;
 	}
 }
